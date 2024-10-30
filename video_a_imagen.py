7cc787f14d94d46e87ca82c0a1e9a4ca79d73efd
Samuel García <38510593+Samuel-GC@users.noreply.github.com>
2024-10-29 21:54:34 -0500

    carga de imagenes

--- video_a_imagen.py
+++ video_a_imagen.py
@@ -1,31 +1,43 @@
 import cv2
 import os
 
-# Ruta del video
-video_path = 'D:/escritorio/reconocimiento_facial/videos/samuel/video_1.mp4'
-# Ruta de la carpeta donde se guardarán las imágenes
-folder_path = 'D:/escritorio/reconocimiento_facial/imagenes/samuel'
-# Crear la carpeta si no existe
+segmentos=20
+
+count = 0
+count_video=0
+user="samuel"
+video_path = f'D:/escritorio/reconocimiento_facial/videos/{user}/'
+folder_path = f'D:/escritorio/reconocimiento_facial/imagenes/{user}'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
-# Cargar el video
-cap = cv2.VideoCapture(video_path)
+for video in os.listdir(video_path):
+    count_video=0
+ 
+    count_segmentos= 0
 
-# Inicializar el contador de imágenes
-count = 0
+    # Cargar el video
+    cap = cv2.VideoCapture(video_path+video)
 
-# Leer el video cuadro por cuadro
-while True:
-    success, frame = cap.read()
-    if not success:
-        break  # Si no hay más cuadros, salir del bucle
+    # Inicializar el contador de imágenes
 
-    # Guardar el cuadro como una imagen
-    cv2.imwrite(f"{folder_path}/frame_{count:04d}.png", frame)
-    count += 1
 
-# Liberar el objeto capturador de video
-cap.release()
+    # Leer el video cuadro por cuadro
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break  # Si no hay más cuadros, salir del bucle
+        if count_segmentos ==segmentos:
 
-print(f"Video segmentado en {count} imágenes guardadas en '{folder_path}'")
+            # Guardar el cuadro como una imagen
+            cv2.imwrite(f"{folder_path}/img_{count:04d}.png", frame)
+            count += 1
+            count_video+=1
+            count_segmentos= 0 
+        else:
+            count_segmentos +=1
+
+    # Liberar el objeto capturador de video
+    cap.release()
+
+    print(f"Video {video} , segmentado en {count_video} imágenes  ")
